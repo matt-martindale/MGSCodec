3673d9380708d778619f60954351a318ebd21935
Matt Martindale <martindalematt18@yahoo.com>
2021-12-01 14:35:04 -0700

    Add Box class for data binding

--- MGSCodec/MGSCodec WatchKit Extension/Utilities/Box.swift
+++ MGSCodec/MGSCodec WatchKit Extension/Utilities/Box.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 final class Box<T> {
-    typealias
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
 }
