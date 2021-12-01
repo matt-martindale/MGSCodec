3daf085f50827b791dcda0a77fdcddb5f2ea085c
Matt Martindale <martindalematt18@yahoo.com>
2021-12-01 14:32:21 -0700

    Add CodecViewModel files

---
+++ MGSCodec/MGSCodec WatchKit Extension/Utilities/Box.swift
@@ -0,0 +1,12 @@
+//
+//  Box.swift
+//  MGSCodec
+//
+//  Created by Matthew Martindale on 12/1/21.
+//
+
+import Foundation
+
+final class Box<T> {
+    typealias
+}
---
+++ MGSCodec/MGSCodec WatchKit Extension/View Models/CodecViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CodecViewModel.swift
+//  MGSCodec
+//
+//  Created by Matthew Martindale on 12/1/21.
+//
+
+import Foundation
+
+final class CodecViewModel {
+    
+}
